c118207a4c6c8ce0725f444fdbb4984264eaed12
Nadun <NAD#LAPTOP>
2025-07-10 00:38:42 +0300

    #FIX-1

---
+++ script.js
@@ -0,0 +1,16 @@
+const navLinks = document.querySelectorAll('.nav-link');
+const sections = document.querySelectorAll('.panel');
+
+function updateActiveLink() {
+    let index = sections.length;
+    while (--index >= 0) {
+        const sectionTop = sections[index].getBoundingClientRect().top;
+        if (sectionTop <= window.innerHeight / 2) {
+            navLinks.forEach(link => link.classList.remove('active'));
+            navLinks[index].classList.add('active');
+            break;
+        }
+    }
+}
+document.querySelector('.main-content').addEventListener('scroll', updateActiveLink);
+window.addEventListener('load', updateActiveLink);
