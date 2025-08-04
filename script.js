bc48276c9d881e9adf4c74e6c1e377037155cb84
Nadun <NAD#LAPTOP>
2025-08-04 23:24:55 +0300

    Links

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ function updateActiveLink() {
 }
 document.querySelector('.main-content').addEventListener('scroll', updateActiveLink);
 window.addEventListener('load', updateActiveLink);
+
+fetch('data/projects.json')
+    .then(response => response.json())
+    .then(data => {
+        // Helper function
+        function renderProjects(sectionId, projects) {
+            const section = document.querySelector(`#${sectionId} .panel-content`);
+            projects.forEach(project => {
+                const div = document.createElement('div');
+                div.classList.add('project');
+                div.innerHTML = `
+          <a href="${project.link}" target="_blank">
+            <img src="${project.image}" alt="${project.title}">
+            <p><strong>${project.title}</strong><br>${project.description}</p>
+          </a>
+        `;
+                section.appendChild(div);
+            });
+        }
+
+        renderProjects('featured', data.featured);
+        renderProjects('brand', data.brand);
+        renderProjects('animation', data.animation);
+        renderProjects('cgi', data.cgi);
+    });
